ca320a20422d2e5722d797ae8393d1110a6b5f54
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2026-06-10 16:34:29 -0400

    fix quotes

--- terraform/evaluate-01/main.tf
+++ terraform/evaluate-01/main.tf
@@ -47,12 +47,12 @@ resource "null_resource" "create_dataset" {
     command = <<-EOT
       set -e
 
-      ACCOUNT_ID=$(curl -s -X GET "https://app.launchdarkly.com/api/v2/account" \
+      ACCOUNT_ID=$(curl -s -X GET 'https://app.launchdarkly.com/api/v2/account' \
           -H "Authorization: $LAUNCHDARKLY_ACCESS_TOKEN" | jq -r '._id')
-      MEMBER_EMAIL="instruqt%2B${LD_PROJECT_KEY}@launchdarkly.com"
-      MEMBER_ID=$(curl -s -X GET "https://app.launchdarkly.com/api/v2/members?filter=email:$MEMBER_EMAIL" \
+      MEMBER_EMAIL='instruqt%2B${var.project_key}@launchdarkly.com'
+      MEMBER_ID=$(curl -s -X GET 'https://app.launchdarkly.com/api/v2/members?filter=email:$MEMBER_EMAIL' \
           -H "Authorization: $LAUNCHDARKLY_ACCESS_TOKEN" | jq -r '.items[0]._id')
-      LD_PROJECT_ID=$(curl -s -X GET "https://app.launchdarkly.com/api/v2/projects/$LD_PROJECT_KEY" \
+      LD_PROJECT_ID=$(curl -s -X GET 'https://app.launchdarkly.com/api/v2/projects/$LD_PROJECT_KEY' \
           -H "Authorization: $LAUNCHDARKLY_ACCESS_TOKEN" | jq -r '._id')
 
       CREATE_RESPONSE=$(curl -fsS -X POST \
